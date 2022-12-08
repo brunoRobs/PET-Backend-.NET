526bcd912e285920a3b20f642fe0789a33b132a4
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2022-12-08 09:35:07 -0300

    Add files via upload

---
+++ SegundoProjetoPet/Services/ClientService.cs
@@ -0,0 +1,45 @@
+﻿using SegundoProjetoPet.Models;
+using SegundoProjetoPet.Repositories;
+
+namespace SegundoProjetoPet.Services
+{
+    public class ClientService
+    {
+        private ClientRepository Repository { get; }
+
+        public ClientService(ClientRepository repository)
+        {
+            Repository = repository;
+        }
+
+        public bool Add(Client client)
+        {
+            return Repository.Add(client);
+        }
+
+        public bool Remove(string cpf)
+        {
+            return Repository.Remove(cpf);
+        }
+
+        public bool Remove(ulong id)
+        {
+            return Repository.Remove(id);
+        }
+
+        public Client? Get(string cpf)
+        {
+            return Repository.Get(cpf);
+        }
+
+        public Client? Get(ulong id)
+        {
+            return Repository.Get(id);
+        }
+
+        public List<Client> Register()
+        {
+            return Repository.Register();
+        }
+    }
+}
