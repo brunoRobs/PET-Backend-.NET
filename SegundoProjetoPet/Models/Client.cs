5a8efaed2eb9edb638ec5cae066d03cd4b07ad48
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2022-12-08 18:19:40 -0300

    Update Client.cs

--- SegundoProjetoPet/Models/Client.cs
+++ SegundoProjetoPet/Models/Client.cs
@@ -7,6 +7,11 @@
         public string Email { get; set; }
 
         private string Password { get; set; }
+        
+        public override string ToString()
+        {
+            return $"Name: {this.Name}\nCPF: {this.Cpf}\nId: {this.Id}\nEmail: {this.Email}";
+        }
 
         public Client(string name, string cpf, string email, string password) : base(name, cpf)
         {
