c0506fd1b9c73fb9872fdcee341faace30115786
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2022-12-08 19:12:40 -0300

    Update ClientController.cs

--- SegundoProjetoPet/Controllers/ClientController.cs
+++ SegundoProjetoPet/Controllers/ClientController.cs
@@ -12,56 +12,35 @@ namespace SegundoProjetoPet.Controllers
         private static ClientService service = new ClientService(new Repositories.ClientRepository());
 
         [HttpPost]
-        public IActionResult Add(Client client)
+        public IActionResult Add(string name, string cpf, string email, string password)
         {
-            if (service.Add(client))
+            if (service.Add(new Client(name,cpf,email,password)))
             {
                 this.Ok(new Message(10, "Succesfully added!"));
             }
             return this.Ok(new Message(11, "Client already existent."));
         }
 
-        [HttpDelete("{cpf}")]
-        public IActionResult Remove(string cpf)
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
         {
-            if (service.Remove(cpf))
+            if (service.Remove(id))
             {
                 return this.Ok(new Message(20, "Succesfully excluded!"));
             }
-            return this.Ok(new Message(21, "Client not found or already deleted."));
+            return this.Ok(new Message(22, "Client not found or already deleted."));
         }
 
-        //[HttpDelete("{id}")]
-        //public IActionResult Remove(int id)
-        //{
-        //    if (service.Remove(id))
-        //    {
-        //        return this.Ok(new Message(20, "Succesfully excluded!"));
-        //    }
-        //    return this.Ok(new Message(22, "Client not found or already deleted."));
-        //}
-
-        [HttpGet("{cpf}")]
-        public IActionResult Get(string cpf)
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
         {
-            Client? client = service.Get(cpf);
-            if (client == null)
+            Client? client = service.Get(id);
+            if(client == null)
             {
-                return this.Ok(new Message(23, "Client not found."));
+                return this.Ok(new Message(24, "Client not found."));
             }
             return this.Ok(client);
         }
-
-        //[HttpGet("{id}")]
-        //public IActionResult Get(int id)
-        //{
-        //    Client? client = service.Get(id);
-        //    if(client == null)
-        //    {
-        //        return this.Ok(new Message(24, "Client not found."));
-        //    }
-        //    return this.Ok(client);
-        //}
 
         [HttpGet]
         public IActionResult Register()
