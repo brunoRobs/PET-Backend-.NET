abcc10257b46b189a81b974f5f55ce15bfac07b1
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2022-12-08 18:27:22 -0300

    Update Client.cs

--- SegundoProjetoPet/Models/Client.cs
+++ SegundoProjetoPet/Models/Client.cs
@@ -2,7 +2,7 @@
 {
     public class Client : Person, ISystem, IAuthenticator
     {
-        public double Id { get; set; }
+        public int Id { get; set; }
 
         public string Email { get; set; }
 
