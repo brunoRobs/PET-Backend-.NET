8c47668cbf46b43616c1547657a6af634be41b9a
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2022-12-08 09:14:43 -0300

    Primeiro commit do segundo projeto

---
+++ Models/IAuthenticator.cs
@@ -0,0 +1,7 @@
+﻿namespace SegundoProjetoPet.Models
+{
+    public interface IAuthenticator
+    {
+        bool Authenticate(string email, string password);
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace SegundoProjetoPet.Models
+{
+    public abstract class Person
+    {
+        public string Cpf { get; set; }
+
+        private string Name { get; set; }
+
+        protected Person(string name, string cpf)
+        {
+            this.Cpf = cpf;
+            this.Name = name;
+        }
+    }
+}
