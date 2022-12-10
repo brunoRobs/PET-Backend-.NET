73b0f1530cf83b9760e8d1a3172b2ca979971228
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2022-12-10 09:23:08 -0300

    Update Client.cs

--- SegundoProjetoPet/Models/Client.cs
+++ SegundoProjetoPet/Models/Client.cs
@@ -1,6 +1,6 @@
 ﻿namespace SegundoProjetoPet.Models
 {
-    public class Client : ISystem, IAuthenticator
+    public class Client : ISystem
     {
         public string? Name { get; set; }
 
@@ -9,30 +9,17 @@
         public int Id { get; set; }
 
         public string? Email { get; set; }
-
-        private string? Password { get; set; }
-
-        public override string ToString()
-        {
-            return $"Name: {this.Name}\nCPF: {this.Cpf}\nId: {this.Id}\nEmail: {this.Email}";
-        }
         
         public Client()
         {
             
         }
 
-        public Client(string name, string cpf, string email, string password)
+        public Client(string name, string cpf, string email)
         {
             this.Name = name;
             this.Cpf = cpf;
             this.Email = email;
-            this.Password = password;
-        }
-
-        public bool Authenticate(string email, string password)
-        {
-            return this.Email == email && this.Password == password;
         }
     }
 }
