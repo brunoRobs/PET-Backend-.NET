ce2520d1d6adf6a043856d780863bb566ed31890
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2022-12-10 08:45:42 -0300

    Add files via upload

---
+++ SegundoProjetoPet/Models/Client.cs
@@ -0,0 +1,33 @@
+﻿namespace SegundoProjetoPet.Models
+{
+    public class Client : ISystem, IAuthenticator
+    {
+        public string Name { get; set; }
+
+        public string Cpf { get; set; }
+
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        private string Password { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}\nCPF: {this.Cpf}\nId: {this.Id}\nEmail: {this.Email}";
+        }
+
+        public Client(string name, string cpf, string email, string password)
+        {
+            this.Name = name;
+            this.Cpf = cpf;
+            this.Email = email;
+            this.Password = password;
+        }
+
+        public bool Authenticate(string email, string password)
+        {
+            return this.Email == email && this.Password == password;
+        }
+    }
+}
