39989483afcf45d94c072c3e76d4faad5aaff74e
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2022-12-11 09:37:10 -0300

    Update services

--- SegundoProjetoPet/Services/ClientService.cs
+++ SegundoProjetoPet/Services/ClientService.cs
@@ -3,33 +3,8 @@ using SegundoProjetoPet.Repositories;
 
 namespace SegundoProjetoPet.Services
 {
-    public class ClientService
+    public class ClientService : Service<Client, ClientRepository>
     {
-        private ClientRepository Repository { get; }
-
-        public ClientService(ClientRepository repository)
-        {
-            Repository = repository;
-        }
-
-        public bool Add(Client client)
-        {
-            return Repository.Add(client);
-        }
-
-        public bool Remove(int id)
-        {
-            return Repository.Remove(id);
-        }
-
-        public Client? Get(int id)
-        {
-            return Repository.Get(id);
-        }
-
-        public List<Client> Register()
-        {
-            return Repository.Register();
-        }
+        public ClientService(ClientRepository repository) : base(repository) { }
     }
 }
---
+++ SegundoProjetoPet/Services/EmployeeService.cs
@@ -0,0 +1,10 @@
+﻿using SegundoProjetoPet.Models;
+using SegundoProjetoPet.Repositories;
+
+namespace SegundoProjetoPet.Services
+{
+    public class EmployeeService : Service<Employee, EmployeeRepository>
+    {
+        public EmployeeService(EmployeeRepository repository) : base(repository) { }
+    }
+}
